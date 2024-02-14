dbd9e53de066729bb447497cb1e26382535a63f7
conno234 <135058788+conno234@users.noreply.github.com>
2024-02-14 10:54:26 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+Let's look at a basic example of how we can use psycopg2 within flask.
+# Import packages
+from flask import Flask
+import psycopg2
+# Initialize the app
+app = Flask(__name__)
+# Create index route
+@app.route("/", methods=["GET"])
+def index():
+  return "You made it! The API is working."
+# Create data route
+@app.route("/data", methods=["GET"])
+def data():
+  # Connect to the database
+  conn = psycopg2.connect(
+    host="localhost",
+    database="my_database",
+    user="postgres,
+    password="my_password",
+    port=5432,
+  )
+  # Retrieve data
+  with conn.cursor() as cur:
+    # Query to get data
+    cur.execute("SELECT ST_AsGeoJSON(my_table.*)::json FROM my_table;")
+    # Fetch
+    data = cur.fetchall()
+  # Do some processing here (if needed)
+  # ...
+  # Close the connection
+  conn.close()
+  # Return the data
+  return data
+# Run the Flask app, when the file is run
+if __name__ == "__main__":
+app.run(debug=True, host="0.0.0.0", port=8080)
