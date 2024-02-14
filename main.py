d9715281c0f95ffa33ca353be5c26a6743063047
conno234 <135058788+conno234@users.noreply.github.com>
2024-02-14 15:35:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-sudo apt-get install build-dep python-psycopg2
-pip install psycopg2-binary
+m flask import Flask 
+# Initialize the app 
+app = Flask(__name__) 
+# This is our index route --- when we go to the main URL, this is what gets returned 
+# In this case, we also specify that this is a 'GET' HTTP request method 
+@app.route("/", methods=["GET"]) 
+def index(): 
+    return "You made it! The API is working." 
+# Run the Flask app, when the file is run 
+if __name__ == "__main__": 
+    app.run(debug=True, host="0.0.0.0", port=8080) 
