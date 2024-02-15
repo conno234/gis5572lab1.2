a8b421d4ce0215029cf1c9d1546060a898c20a78
conno234 <135058788+conno234@users.noreply.github.com>
2024-02-14 22:24:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 import psycopg2
 import json
 
-# Initialize Flask app
+# Define Flask app
 app = Flask(__name__)
 
 # Database connection parameters
@@ -18,25 +18,16 @@ def fetch_geom_as_geojson(table_name, geom_column, db_params):
     conn = psycopg2.connect(**db_params)
     cur = conn.cursor()
     cur.execute(f"SELECT ST_AsGeoJSON({geom_column}) FROM {table_name} LIMIT 1")
-    geojson_string = cur.fetchone()[0]
+    geojson = cur.fetchone()[0]
     conn.close()
-    # Decode JSON string twice to get the GeoJSON object
-    geojson_object = json.loads(json.loads(geojson_string))
-    # Modify the GeoJSON object to follow the specified format
-    feature = {
-        "type": "Feature",
-        "geometry": geojson_object,
-        "properties": {}
-    }
-    return feature
+    return geojson
 
-@app.route('/')
+@app.route('/get_geojson')
 def get_geojson():
     table_name = "labtable"
     geom_column = "geom"
     geojson = fetch_geom_as_geojson(table_name, geom_column, db_params)
-    # Convert Python dictionary to JSON string
-    return json.dumps(geojson)
-
+    geojson_object = json.loads(geojson)
+    return jsonify(geojson_object)
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
